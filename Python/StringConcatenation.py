b4f742047543f096a4231280157b73a9c7616afc
Kris <fragranceofbook@gmail.com>
2020-12-19 10:55:50 +0530

    Adding Python Exercise Files

---
+++ Python/StringConcatenation.py
@@ -0,0 +1,10 @@
+
+
+Name = 'Amit'
+
+Last_Name = 'Yadav'
+
+Age = 00251 
+
+
+print("My Name is" + " "  + Name + " " + Last_Name +  " " + "And My" + str(Age))
---
+++ Python/input.py
@@ -0,0 +1,7 @@
+age = int(input(" What is your age"))
+
+
+if age > 18:
+    print(" You can vote in the election")
+else:
+    print ("Wait till you become above 18")
